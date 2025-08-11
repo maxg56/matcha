daaa2604e8eabdb233ef627417c0887e28014a54
maxg56 <mgendrot56@gmail.com>
2025-08-11 13:04:21 +0200

    feat: add Dockerfile for frontend development setup and update package manager in package.json

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <body>
+      <button>Click me</button>
+      <h1>Hello, world!</h1>
+    </body>
   </StrictMode>,
 )
