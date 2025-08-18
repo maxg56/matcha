8c2c9d2f98b4389cb14c8f2f12faa121a823b276
vedr-folnir <vedrfolnir76@gmail.com>
2025-08-18 14:28:57 +0200

    feat(flame color): ajout des differents types de flames + affichage flamme plus importante

--- frontend/src/hooks/NotifImage.ts
+++ frontend/src/hooks/NotifImage.ts
@@ -1,23 +1,30 @@
 import { useState, useEffect } from "react";
 
+
+
+
 export function Notification() {
+    function randomInt(max: number): number {
+        return Math.floor(Math.random() * max);
+    }
+    let [seen, setSeen] = useState(false);
     const [notifications, setNotifications] = useState<string[]>([]);
-    const [hasNotification, setHasNotification] = useState(false);
+    const [Color, setColor] = useState(0)
 
     useEffect(() => {
         const interval = setInterval(() => {
             const newNotif = `Nouvelle notification ${notifications.length + 1}`;
-            setNotifications(prev => [...prev, newNotif]);
-            setHasNotification(true);
-        }, 5000);
+            setColor(randomInt(5));
+            setNotifications(prev => [...prev, [newNotif, Color]]);
+            setSeen(false)
+        }, 2000);
 
         return () => clearInterval(interval);
     }, [notifications.length]);
 
     const clearNotifications = () => {
-        setNotifications([]);
-        setHasNotification(false);
+        setNotifications([]);    
     };
 
-    return { notifications, hasNotification, clearNotifications };
+    return { notifications, clearNotifications, seen, setSeen};
 }
