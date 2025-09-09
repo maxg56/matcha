000d5fd5c6ab75508364e8ef1769d311cc386bd9
maxg56 <mgendrot56@gmail.com>
2025-09-09 23:28:34 +0200

    refactor: Remove CreatedAt field from Tag struct to simplify model

--- api/user-service/src/models/tag.go
+++ api/user-service/src/models/tag.go
@@ -1,12 +1,9 @@
 package models
 
-import "time"
-
 // Tag represents user interest tags
 type Tag struct {
 	ID        uint      `gorm:"primaryKey;column:id" json:"id"`
 	Name      string    `gorm:"column:name;uniqueIndex;not null" json:"name"`
-	CreatedAt time.Time `gorm:"column:created_at;default:CURRENT_TIMESTAMP" json:"created_at"`
 }
 
 func (Tag) TableName() string { return "tags" }
