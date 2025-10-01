2836be00b6665e476ca74524e35ef4c9288f7075
vedr-folnir <vedrfolnir76@gmail.com>
2025-10-01 09:25:51 +0200

    fix(map ui): deplacement du 'nb match sur la carte' de 20px vers le haut

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -16,9 +16,9 @@ export default function MapPage() {
         <div className="flex-1 flex flex-col">
           {/* En-tête */}
           <div className="bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 px-6 py-4">
-            <div className="flex items-center gap-4">
-              <h1 className="text-2xl font-bold">Carte de vos matches</h1>
-            </div>
+            {/* <div className="flex items-center gap-4"> */}
+              {/* <h1 className="text-2xl font-bold">Carte de vos matches</h1> */}
+            {/* </div> */} 
 
             {/* Informations utilisateur sélectionné */}
             {selectedUser && (
